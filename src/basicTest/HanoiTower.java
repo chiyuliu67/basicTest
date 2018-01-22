e5c93a44ddef56ddfe8e5cd42c80ac1f5d4fc756
Louis.Liu <1672879+chiyuliu67@users.noreply.github.com>
2018-01-22 16:12:20 -0500

    Initial commit.

---
+++ src/basicTest/HanoiTower.java
@@ -0,0 +1,20 @@
+package basicTest;
+
+public class HanoiTower {
+
+	public static void main(String[] args) {
+
+		processTower( "a", "b", "c", 7, 0 );
+	}
+
+	public static int processTower( String source, String intermediate, String target, int count, int stepNo ) {
+
+		if ( count != 0 ) {
+		
+			stepNo = processTower( source, target, intermediate, count - 1, stepNo );
+			System.out.println( ++stepNo + ": " + source + " -> " + target + " ( " + count + " )" );
+			stepNo = processTower( intermediate, source, target, count - 1, stepNo );
+		}
+		return stepNo;
+	}
+}
